c1b3803e4addd62d5b0ae428fb841ad61a95c12a
mrbighokage <mrbighokage@gmail.com>
2015-09-27 14:33:31 +0300

    fix

--- frontend/config/bootstrap.php
+++ frontend/config/bootstrap.php
@@ -1,3 +1,3 @@
 <?php
-Yii::setAlias('theme_base', '/themes/base/');
-Yii::setAlias('theme_patch_base', dirname(__DIR__) . '/web/themes/base/');
+Yii::setAlias('theme_basic', '/themes/basic/');
+Yii::setAlias('theme_patch_basic', dirname(__DIR__) . '/web/themes/basic/');
